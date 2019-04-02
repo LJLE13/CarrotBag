88f89b31fc1a25dc3a203492daf9df93c5fce433
Matt Girdler <matt.girdler@digital.landregistry.gov.uk>
2019-04-02 14:40:29 +0100

    Use uuids for cards/columns. Add RemoveCard function

--- src/components/Context/index.js
+++ src/components/Context/index.js
@@ -1,79 +1,96 @@
 import React, {Component} from 'react';
 
+const uuidv4 = require('uuid/v4');
 const CarrotBagContext = React.createContext();
 
 export class Provider extends Component {
-    state = {
+  constructor(props) {
+    super(props);
+    this.handleAddColumn = this.handleAddColumn.bind(this);
+    this.handleRemoveColumn = this.handleRemoveColumn.bind(this);
+    this.handleAddCard = this.handleAddCard.bind(this);
+    this.handleRemoveCard = this.handleRemoveCard.bind(this);
+    this.state = {
+      columns: [
+        {
+          name: "Column One",
+          id: uuidv4(),
+          cards: [
+            {
+              name: "Card One",
+              id: uuidv4()
+            }
+          ]
+        },
+        {
+          name: "Column Two",
+          id: uuidv4(),
+          cards: []
+        }
+      ]
+    };
+  };
+
+  handleAddColumn(name) {
+    this.setState(prevState => {
+      return {
         columns: [
+          ...prevState.columns,
           {
-            name: "Column One",
-            id: 1,
-            cards: [
-              {
-                name: "Card One",
-                id: 1
-              }
-            ]
-          },
-          {
-            name: "Column Two",
-            id: 2,
+            name,
+            id: uuidv4(),
             cards: []
           }
         ]
       }
+    })
+  }
+
+  handleRemoveColumn(id) {
+    this.setState(prevState => {
+      return {
+        columns: prevState.columns.filter(p => p.id !== id)
+      };
+    });
+  }
     
-      prevColumnId = 2;
-      prevCardId = 1;
-    
-      handleAddColumn = (name) => {
-        this.setState( prevState => {
-          return {
-            columns: [
-              ...prevState.columns,
-              {
-                name,
-                id: this.prevColumnId +=1,
-                cards: []
-              }
-            ]
-          }
-        })
-      }
-    
-      handleRemoveColumn = (id) => {
-        this.setState(prevState => {
-          return {
-            columns: prevState.columns.filter( p => p.id !== id)
-          };
-        });
-      }
-    
-      handleAddCard = (name, columnIndex) => {
-        this.setState(prevState => {
-            return (
-                prevState.columns[columnIndex].cards.push({
-                    name,
-                    id: this.prevCardId +=1
-                })
-            );
-        });
-    }
-    
-      render() {
+  handleAddCard(name, columnIndex) {
+    this.setState(prevState => {
         return (
-          <CarrotBagContext.Provider value={{
-            columns: this.state.columns,
-            actions: {
-              removeColumn: this.handleRemoveColumn,
-              addCard: this.handleAddCard,
-              addColumn: this.handleAddColumn
-            }
-            }}>
-            { this.props.children }
-          </CarrotBagContext.Provider>
+            prevState.columns[columnIndex].cards.push({
+                name,
+                id: uuidv4()
+            })
         );
-    }
+    });
+  }
+
+  handleRemoveCard(columnIndex, cardId) {
+    this.setState(prevState => {
+      let updatedColumn = prevState.columns[columnIndex];
+      updatedColumn.cards = updatedColumn.cards.filter(card => card.id !== cardId);
+      prevState.columns[columnIndex] = updatedColumn;
+      return {
+        columns: prevState.columns
+      }
+    });
+  }
+    
+  render() {
+    return (
+      <CarrotBagContext.Provider value={{
+        columns: this.state.columns,
+        actions: {
+          addColumn: this.handleAddColumn,
+          removeColumn: this.handleRemoveColumn,
+          addCard: this.handleAddCard,
+          removeCard: this.handleRemoveCard
+        }
+        }}>
+        { this.props.children }
+      </CarrotBagContext.Provider>
+    );
+  }
 }
 
 export const Consumer = CarrotBagContext.Consumer;

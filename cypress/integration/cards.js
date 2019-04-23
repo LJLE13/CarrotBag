b6916797bd5817d5e600312cecbf0bf93e5d0115
Aaron Flynn <aaron.flynn@digital.landregistry.gov.uk>
2019-04-23 15:51:41 +0100

    Added card craetion / deletion functions. Added a add card test

--- cypress/integration/cards.js
+++ cypress/integration/cards.js
@@ -3,39 +3,69 @@
  */
 
 describe('Carrotbag is started', function() {
-    it('Checks Carrotbag is started', function() {
-        cy.visit('http://localhost:3000')
+    it('Checks Carrotbag starts', function() {
+        cy.visit('/')
         cy.contains('Column')
     })
 })
 
 describe('Carrotbag Add Card Test', function() {
-    it('finds the content "type"', function() {
-        cy.visit('http://localhost:3000')
+    it('Adds a card', function() {
+        cy.visit('/')
         cy.wait(300)
-        cy.get('input:first').should('have.attr', 'placeholder', 'Add new card')
-            .click()
-            .type('Hello world card {enter}')
-        cy.contains('Hello world card')
+        add_card('Add card Test')
     })
 })
 
 describe('Carrotbag Delete Card Test', function() {
-    it('finds the content "type"', function() {
+    it('Removes a card', function() {
         // Create a card to delete
-        cy.visit('http://localhost:3000')
+        cy.visit('/')
         cy.wait(300)
-        cy.get('input:first').should('have.attr', 'placeholder', 'Add new card')
-            .click()
-            .type('Delete me card {enter}')
-        cy.contains('Delete me card')
+        add_card('Delete card test')
 
         // Delete the card
         cy.wait(100)
-        cy.contains('Delete me card').click()
+        delete_card('Delete card test')
 
         // Assert Card is removed
-       // expect(cy.contains('Delete me card')).to.be.false
+        cy.get('Delete card test').should('not.exist')
     })
 })
 
+describe('Carrotbag Add Column Test', function() {
+    it('Adds a column', function() {
+        cy.visit('/')
+        cy.wait(300)
+        add_column('Test Column')
+    })
+})
+
+describe('Carrotbag Delete Column Test', function() {
+    it('Deletes a column', function() {
+        cy.visit('/')
+        cy.wait(300)
+        add_column('Delete Column test')
+
+        // Test to explode here
+        assert(false)
+    })
+})
+
+function add_card(card_text) {
+    cy.get('input:first').should('have.attr', 'placeholder', 'Add new card')
+        .click()
+        .type(card_text + '{enter}')
+    cy.contains(card_text)
+}
+
+function delete_card(column_text) {
+    cy.contains(column_text).click()
+}
+
+function add_column(column_text) {
+    cy.get('input:last').should('have.attr', 'placeholder', 'Add new column')
+        .click()
+        .type(column_text + '{enter}')
+    cy.contains(column_text)
+}
